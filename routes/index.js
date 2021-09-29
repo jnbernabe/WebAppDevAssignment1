ed8684c6297265fd958e37e7dacaa00cb182c45a
Jamaal Bernabe <jnbernabe@gmail.com>
2021-09-29 14:58:59 -0400

    Working on Form

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get("/home", function (req, res, next) {
 
 /* GET About Us page. */
 router.get("/about", function (req, res, next) {
-  res.render("index", { title: "About" });
+  res.render("index", { title: "About Me" });
 });
 
 /* GET Products page. */
@@ -28,7 +28,7 @@ router.get("/services", function (req, res, next) {
 
 /* GET Contact Us page. */
 router.get("/contact", function (req, res, next) {
-  res.render("index", { title: "Contact" });
+  res.render("index", { title: "Contact Me" });
 });
 
 module.exports = router;
